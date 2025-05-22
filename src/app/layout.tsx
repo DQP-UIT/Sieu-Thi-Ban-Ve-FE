c61c213b34574068bff9d0eed97e05ffc0432a74
Present111 <161811020+Present111@users.noreply.github.com>
2025-05-22 08:54:46 +0700

    ChatBot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import ChatWidget from "@/components/ChatWidget";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,8 @@ export default function RootLayout({
       >
         <SessionProvider>
           {children}
+
+          <ChatWidget />
         </SessionProvider>
       </body>
     </html>
